6cbf1226e379d07bb93e11fce6dc8e90aa334437
David Byrd <david@byrdhou.se>
2013-03-07 01:38:41 -0500

    Update i5.js

--- i5.js
+++ i5.js
@@ -1,30 +1,62 @@
 #!/usr/bin/env node
 var fs = require('fs'),
-    spawn = require('child_process').spawn,
-    ssh;
+    input = require('prompt'),
+    cp = require('child_process'),
+    spawn = cp.spawn,
+    exec = cp.exec;
 
+input.message = 'i5';
 
 var methods = {
   login: function (username) {
-    username ?
-      ssh = spawn('ssh', ['-tt', username + '@i5.nyu.edu'], { stdio: 'inherit' })
-    : console.log('Please provide your username.');
-  },
-  connect: function () {
+    var self = this
 
-  },
-  upload: function (folder) {
+    if (username)
+      fs.writeFile('.i5', username)
+    else
+      fs.readFile('.i5', function (error, username) {
+        error ?
+          self.getUsername('login', username)
+        : spawn('ssh', ['-tt', username + '@i5.nyu.edu'], { stdio: 'inherit' })
+      });
+
+  }
+
+, upload: function (folder) {
+
+    var self = this
+
+    fs.readFile('.i5', function (error, username) {
+      error ?
+        self.getUsername('upload', folder)
+      : scp = spawn('scp', ['-r', folder, username + '@i5.nyu.edu:public_html'], { stdio: 'inherit'})
+      
+    });
+
+  }
+
+, run: function (folder) {
     
-  },
-  run: function (folder) {
-    this.connect();
-    this.upload(folder);
+  }
 
+, reset: function () {
+    exec('rm .i5')
+  }
+
+, getUsername: function (origin, origin_arg) {
+
+    var self = this
+
+    input.start();
+
+    input.get(['username'], function (error, result) {
+        fs.writeFileSync('.i5', result.username)
+        self[origin](origin_arg)
+    });
   }
 };
 
-//execute correct method
 !function (args) {
-  var command = args.shift();
+  var command = args.shift()
   methods[command] && methods[command].apply(methods, args)
 }(process.argv.slice(2))
